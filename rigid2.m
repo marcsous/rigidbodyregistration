31fee5514042113e0dcdf3d73cd64a909850e091
Mark Bydder <17042435+marcsous@users.noreply.github.com>
2026-09-04 14:28:35 -0700

    Add files via upload

--- rigid2.m
+++ rigid2.m
@@ -1,78 +1,131 @@
-function [im2 delta] = rigid2(im1,im2,n)
+function [im2 delta] = rigid2(im1,im2,voxel)
 %[im2 delta] = rigid2(im1,im2,n)
 % aligns im2 with im1 using 2d rigid body registration
 %  -accepts 2D image, or stack of 2d images [nx ny (ns)]
-%  -n (optional) is the number of histogram bins (max 256)
+%  -voxel (optional) is the voxel dimensions (mm) [dx dy]
 %  -im2 is returned registered using interp2
 %  -delta is the shifts [dx dy zrot]
 %
 % Ref: Lu et al (doi.org/10.1016/j.compmedimag.2007.12.001)
 %
 %% handle inputs
-if ~isreal(im1) || ~isreal(im2) || nnz(isfinite(im1)==0) || nnz(isfinite(im2)==0)
-    error('im1 and im2 must be real valued.');
+if ~isreal(im1) || ~isreal(im2) || nnz(~isfinite(im1)) || nnz(~isfinite(im2))
+    error('im1 and im2 must be finite and real.');
 end
-if ~isequal(size(im1),size(im2)) || ndims(im1)<2
-    error('im1 and im2 must be 2d-3d arrays of the same size.');
-end
-
-% Terrel-Scott rule (max 256)
-if ~exist('n','var') || isempty(n)
-    n = min(ceil((2*numel(im1))^(1/3)),256); 
-elseif numel(n)~=1 || n<1 || n>256 || mod(n,1)
-    error('number of histogram bins invalid.');
+if ~exist('voxel','var') || isempty(voxel)
+    voxel = ones(2,1);
+elseif numel(voxel)~=2 || ~isreal(voxel) || any(voxel<=0) || any(~isfinite(voxel))
+    error('voxel must be a real 2-element vector.');
 end
 
-%% convert to n bins
-uim1 = im2uint8(im1,n);
-uim2 = im2uint8(im2,n);
+% isotropic resolution unit used for registration 
+unit = mean(voxel) ; % 1.0; 
 
-% faster on gpu
+%% copy of input arrays for registration
 try
-    uim1 = gpuArray(uim1);
-    uim2 = gpuArray(uim2);
+    uim1 = gpuArray(im1);
+    uim2 = gpuArray(im2);
+catch
+    uim1 = im1;
+    uim2 = im2;
 end
+
+% interpolate to "1x1x1" resolution
+for dim = 1:2
+    N = round(size(im2,dim) * voxel(dim) / unit);
+    uim1 = interpft(uim1,N,dim);
+    uim2 = interpft(uim2,N,dim);
+end
+
+%% convert to n bins - Terrel-Scott rule (max 256)
+n = min(ceil((2*numel(uim2))^(1/3)),256); 
+
+uim1 = im2uint8(uim1,n);
+uim2 = im2uint8(uim2,n);
 
 %% calcuate transforms: [dx dy zrot]
-opts = optimoptions('fminunc','SpecifyObjectiveGradient',true,'TolFun',1e-4,'display','iter');
+t = tic;
 
-cost = @(delta)hpv(uim1,uim2,delta,n);
+opts = optimoptions('fminunc','SpecifyObjectiveGradient',true,'display','off');
 
-delta = fminunc(cost,[0 0 0],opts)
+cost = @(delta)hpv(uim1,uim2,delta,n,[1 1]);
 
-%% apply transforms
-[nx ny ns] = size(im1);
+delta = fminunc(cost,[0 0 0],opts);
 
-% get coordinates of im2
-[~,~,x2,y2] = cost(delta);
-clear cost uim1 uim2 opts
+fprintf('%s: %+.2f %+.2f %+.2f (%.1f sec)\n',mfilename,delta,toc(t));
 
-% wrap edges and use 1-based indexing
-x2 = mod(x2,nx)+1;
-y2 = mod(y2,ny)+1;
-
-% interpolate im2 to im1 
-for s = 1:ns
-    
-    % current slice
-    curr = cast(im2(:,:,s),'like',x2);
-
-    % preserve bounds (e.g. nonnegative)
-    [S L] = bounds(reshape(curr,[],1));
-
-    % spline/cubic may fail on gpu
-    try
-        curr = interp2(curr,y2,x2,'cubic',0);
-    catch
-        curr = interp2(curr,y2,x2,'linear',0);
-    end
-
-    im2(:,:,s) = min(max(curr,S),L);
-
+%% isotropic copies of input arrays for interpolation
+try
+    uim2 = gpuArray(im2);
+catch
+    uim2 = im2;
 end
 
+% interpolate to highest resolution
+for dim = 1:2
+    N = round(size(im2,dim) * voxel(dim) / min(voxel));
+    uim2 = interpft(uim2,N,dim);
+    delta(dim) = delta(dim) * unit / min(voxel);
+end
+
+%% apply the transform to the native grid 
+
+% convert shifts from isotropic to native voxels
+delta(1:2) = delta(1:2) .* unit;
+
+[nx ny ns] = size(im2);
+
+if isa(im2,'gpuArray')
+    nx = gpuArray(nx);
+    ny = gpuArray(ny);
+end
+[x2 y2] = get_coords([nx ny],voxel,delta);
+
+% wrap edges (1-based indexing)
+x2 = reshape(mod(x2,nx)+1,[nx ny]);
+y2 = reshape(mod(y2,ny)+1,[nx ny]);
+
+% preserve bounds (e.g. nonnegative)
+[S L] = bounds(reshape(im2,[],1));
+
+% interpolate 
+for s = 1:ns
+    % cubic may fail on gpu, linear looks lowpass filtered
+    try
+        im2(:,:,s) = interp2(im2(:,:,s),y2,x2,'cubic',0);
+    catch
+        im2(:,:,s) = interp2(im2(:,:,s),y2,x2,'nearest',0);
+    end
+end
+im2 = min(max(im2,S),L);
+
+%% rigid body coordinates on a grid with spacing of "voxel"
+function [x2 y2 P Dz] = get_coords(sz,voxel,delta)
+
+nx = sz(1); ny = sz(2);
+
+% coordinates of im1 (centered at 0 0)
+[x1 y1] = ndgrid(-nx/2:nx/2-1,-ny/2:ny/2-1);
+
+% convert to matrix form
+P = [x1(:) y1(:)] .* reshape(voxel,1,[]);
+
+% ease notation
+sindzrot = sind(delta(3));
+cosdzrot = cosd(delta(3));
+
+% rotation matrix about z-axis
+Rz = [+cosdzrot,+sindzrot;-sindzrot,+cosdzrot];
+
+% coordinates of im2 (centered at nx/2 ny/2)
+x2 = (P*Rz(:,1) + delta(1)) / voxel(1) + nx/2;
+y2 = (P*Rz(:,2) + delta(2)) / voxel(2) + ny/2;
+
+% partial derivative of Rz wrt delta(3)
+Dz = [-sindzrot,+cosdzrot;+cosdzrot,-sindzrot];
+
 %% mutual information by joint histogram estimation (hpv)
-function [fval grad x2 y2] = hpv(im1,im2,delta,n)
+function [fval grad] = hpv(im1,im2,delta,n,voxel)
 
 [nx ny ns] = size(im1);
 
@@ -82,28 +135,14 @@ if isa(im1,'gpuArray')
     ns = gpuArray(ns);
 end
 
-% joint histogram and partial derivatives [dx dy dzrot]
-h = zeros(n,n,4,'like',nx);
-
-% coordinates of im1 (centered at 0 0)
-[x1 y1] = ndgrid(-nx/2:nx/2-1,-ny/2:ny/2-1);
-
-% coordinates of im2 (centered at nx/2 ny/2)
-sindzrot = sin(delta(3) * pi / 180);
-cosdzrot = cos(delta(3) * pi / 180);
-x2 = nx/2 + delta(1) + x1*cosdzrot - y1*sindzrot;
-y2 = ny/2 + delta(2) + x1*sindzrot + y1*cosdzrot;
-
-% early return for coordinates only
-if nargout>2
-    x2 = reshape(x2,[nx ny]);
-    y2 = reshape(y2,[nx ny]);
-    fval = 0; grad = 0; return;
-end
+[x2 y2 P Dz] = get_coords([nx ny],voxel,delta);
 
 % vectorize slices
 im1 = reshape(im1,nx*ny,ns);
 im2 = reshape(im2,nx*ny,ns);
+
+% joint histogram and partial derivatives [dx dy dzrot]
+h = zeros(n,n,4,'like',nx);
 
 % convolution with 4x4 Hann window
 for i = -1:2
@@ -119,24 +158,28 @@ for i = -1:2
         iy = mod(iy,ny);
 
         % histogram indices
-        tmp = im2(1+ix+iy*nx,:); 
-        ind = 1+[im1(:) tmp(:)]; clear ix iy tmp
+        index = im2(1+ix+iy*nx,:); clear ix iy
+        index = 1 + [im1(:) index(:)]; 
         
-        % function (k=1) and partial derivatives (k=2-4)            
+        % function (m=1) and partial derivatives (m=2-4)            
         cosdx = cospi(dx/2); sindx = sinpi(dx/2); clear dx
         cosdy = cospi(dy/2); sindy = sinpi(dy/2); clear dy
 
-        for k = 1:4
-            switch(k)
-                case 1; f(:,:,1) = (1+cosdx).*(1+cosdy);  % f
-                case 2; f(:,:,2) =-(1+cosdy).*sindx*pi/2; % df/dx
-                case 3; f(:,:,3) =-(1+cosdx).*sindy*pi/2; % df/dy
-                case 4; f(:,:,4) =-f(:,:,2).*(x1*sindzrot+y1*cosdzrot)*pi/180 ...
-                                  +f(:,:,3).*(x1*cosdzrot-y1*sindzrot)*pi/180; % df/dzrot            
+        for m = 1:4
+            switch(m)
+                case 1; f = (1+cosdx).*(1+cosdy);               % f
+                case 2; f = (0-sindx).*(1+cosdy)*pi/2/voxel(1); % df/dx
+                case 3; f = (1+cosdx).*(0-sindy)*pi/2/voxel(2); % df/dy
+                case 4; f = sum(F.*(P*Dz),2)*pi/180;            % df/dzrot
             end
 
-            tmp = repmat(reshape(f(:,:,k),[],1),ns,1);
-            h(:,:,k) = h(:,:,k) + accumarray(ind,tmp,[n n]); clear tmp
+            % help with out-of-memory issues
+            if m==2; F(:,1) = f; clear sindx cosdy; end
+            if m==3; F(:,2) = f; clear cosdx sindy; end
+
+            if ns>1; f = repmat(f,ns,1); end
+            h(:,:,m) = h(:,:,m) + accumarray(index,f,[n n]);
+
         end
 
     end
--- rigid3.m
+++ rigid3.m
@@ -1,110 +1,121 @@
-function [im2 delta] = rigid3(im1,im2,n)
-%[im2 delta] = rigid3(im1,im2,n)
+function [im2 delta] = rigid3(im1,im2,voxel)
+%[im2 delta] = rigid3(im1,im2,voxel)
 % aligns im2 with im1 using rigid body registration
 %  -accepts 3d images or stack of 3d images [nx ny nz (ns)]
-%  -n (optional) is the number of histogram bins (max 256)
+%  -voxel (optional) is the voxel dimensions [dx dy dz]
 %  -im2 is returned registered using interp3
 %  -delta is the shifts [dx dy dz xrot yrot zrot]
 %
 % Ref: Lu et al (doi.org/10.1016/j.compmedimag.2007.12.001)
 %
-%% handle inputs
-if ~isreal(im1) || ~isreal(im2) || nnz(isfinite(im1)==0) || nnz(isfinite(im2)==0)
-    error('im1 and im2 must be real valued.');
+%% setup
+if ~isreal(im1) || ~isreal(im2) || nnz(~isfinite(im1)) || nnz(~isfinite(im2))
+    error('im1 and im2 must be finite and real.');
 end
-if ~isequal(size(im1),size(im2)) || ndims(im1)<3
-    error('im1 and im2 must be 3d-4d arrays of the same size.');
+if ndims(im1)<3 || ndims(im2)<3
+    error('im1 and im2 must be 3d arrays.');
 end
-
-% Terrel-Scott rule (max 256)
-if ~exist('n','var') || isempty(n)
-    n = min(ceil((2*numel(im1))^(1/3)),256); 
-elseif numel(n)~=1 || n<1 || n>256 || mod(n,1)
-    error('number of histogram bins invalid.');
+if ~exist('voxel','var') || isempty(voxel)
+    voxel = [1 1 1];
+elseif numel(voxel)~=3 || ~isreal(voxel) || nnz(voxel<=0) || nnz(~isfinite(voxel))
+    error('voxel must be a real 3-element vector.');
 end
 
-%% convert to n bins
-uim1 = im2uint8(im1,n);
-uim2 = im2uint8(im2,n);
+% isotropic resolution unit used for registration 
+unit = mean(voxel) ; % 1.0; 
 
-% faster on gpu
+%% input arrays at unit isotropic resolution for registration
 try
-    uim1 = gpuArray(uim1);
-    uim2 = gpuArray(uim2);
+    uim1 = gpuArray(im1);
+    uim2 = gpuArray(im2);
+catch
+    uim1 = im1;
+    uim2 = im2;
 end
+
+% interpolate to unit isotropic resolution
+for dim = 1:3
+    N = round(size(im2,dim) * voxel(dim) / unit);
+    uim1 = interpft(uim1,N,dim);
+    uim2 = interpft(uim2,N,dim);
+end
+
+%% convert to n bins - Terrel-Scott rule (max 256)
+n = min(ceil((2*numel(uim2))^(1/3)),256);
+
+uim1 = im2uint8(uim1,n);
+uim2 = im2uint8(uim2,n);
 
 %% calcuate rigid body parameters: [dx dy dz xrot yrot zrot]
-opts = optimoptions('fminunc','SpecifyObjectiveGradient',true,'TolFun',1e-4,'display','iter');
+t = tic;
 
-cost = @(delta)hpv(uim1,uim2,delta,n);
+opts = optimoptions('fminunc','SpecifyObjectiveGradient',true,'display','off');
 
-delta = fminunc(cost,[0 0 0 0 0 0],opts)
+cost = @(delta)hpv(uim1,uim2,delta,n,[1 1 1]);
 
-%% apply the transforms
-[nx ny nz ns] = size(im1);
+delta = fminunc(cost,[0 0 0 0 0 0],opts);
 
-% get coordinates of im2
-[~,~,x2,y2,z2] = cost(delta);
-clear cost uim1 uim2 opts
+fprintf('%s: %+.2f %+.2f %+.2f %+.2f %+.2f %+.2f (%.1f sec)\n',mfilename,delta,toc(t));
 
-% wrap edges and use 1-based indexing
-x2 = mod(x2,nx)+1;
-y2 = mod(y2,ny)+1;
-z2 = mod(z2,nz)+1;
+%% apply the transform to the native grid 
 
-% interpolate im2 to im1
-for s = 1:ns
+% convert shifts from isotropic to native voxels
+delta(1:3) = delta(1:3) .* unit;
 
-    % current slab
-    curr = cast(im2(:,:,:,s),'like',x2);
+[nx ny nz ns] = size(im2);
 
-    % preserve bounds (e.g. nonnegative)
-    [S L] = bounds(reshape(curr,[],1));
-
-    % spline/cubic may fail on gpu
-    try
-        curr = interp3(curr,y2,x2,z2,'cubic',0);
-    catch
-        curr = interp3(curr,y2,x2,z2,'linear',0);
-    end
-
-    im2(:,:,:,s) = min(max(curr,S),L);
-
-end
-
-%% mutual information by joint histogram estimation (hpv)
-function [fval grad x2 y2 z2] = hpv(im1,im2,delta,n)
-
-[nx ny nz ns] = size(im1);
-
-if isa(im1,'gpuArray')
+if isa(im2,'gpuArray')
     nx = gpuArray(nx);
     ny = gpuArray(ny);
     nz = gpuArray(nz);
 end
+[x2 y2 z2] = get_coords([nx ny nz],voxel,delta);
 
-% joint histogram and partial derivatives [dx dy dz dxrot dyrot dzrot]
-h = zeros(n,n,7,'like',nx);
+% wrap edges (1-based indexing)
+x2 = reshape(mod(x2,nx)+1,[nx ny nz]);
+y2 = reshape(mod(y2,ny)+1,[nx ny nz]);
+z2 = reshape(mod(z2,nz)+1,[nx ny nz]);
+
+% preserve bounds (e.g. nonnegative)
+[S L] = bounds(reshape(im2,[],1));
+
+% interpolate 
+for s = 1:ns
+    % cubic may fail on gpu, linear looks lowpass filtered
+    try
+        im2(:,:,:,s) = interp3(im2(:,:,:,s),y2,x2,z2,'cubic',0);
+    catch
+        im2(:,:,:,s) = interp3(im2(:,:,:,s),y2,x2,z2,'nearest',0);
+    end
+end
+im2 = min(max(im2,S),L);
+
+%% rigid body coordinates on a grid with spacing of "voxel"
+function [x2 y2 z2 P Dx Dy Dz] = get_coords(sz,voxel,delta)
+
+nx = sz(1); ny = sz(2); nz = sz(3);
 
 % coordinates of im1 (centered at 0 0 0)
 [x1 y1 z1] = ndgrid(-nx/2:nx/2-1,-ny/2:ny/2-1,-nz/2:nz/2-1);
 
 % convert to matrix form
-P = cat(2,reshape(x1,[],1),reshape(y1,[],1),reshape(z1,[],1)); clear x1 y1 z1
+P = [x1(:) y1(:) z1(:)] .* reshape(voxel,1,[]);
 
-% ease notation, convert to radians
-s4 = sin(delta(4) * pi / 180);
-s5 = sin(delta(5) * pi / 180);
-s6 = sin(delta(6) * pi / 180);
-c4 = cos(delta(4) * pi / 180);
-c5 = cos(delta(5) * pi / 180);
-c6 = cos(delta(6) * pi / 180);
+% ease notation
+s4 = sind(delta(4)); c4 = cosd(delta(4));
+s5 = sind(delta(5)); c5 = cosd(delta(5));
+s6 = sind(delta(6)); c6 = cosd(delta(6));
 
 % rotation matrices
-Rx = [1,0,0;0,c5,s5;0,-s5,c5]; % about x-axis
-Ry = [c4,0,-s4;0,1,0;s4,0,c4]; % about y-axis
-Rz = [c6,s6,0;-s6,c6,0;0,0,1]; % about z-axis
+Rx = [1,0,0; 0,c5,s5; 0,-s5,c5]; % about x-axis
+Ry = [c4,0,-s4; 0,1,0; s4,0,c4]; % about y-axis
+Rz = [c6,s6,0; -s6,c6,0; 0,0,1]; % about z-axis
 Rxyz = Rx * Ry * Rz;
+
+% coordinates of im2 (centered at nx/2 ny/2 nz/2)
+x2 = (P*Rxyz(:,1) + delta(1)) / voxel(1) + nx/2;
+y2 = (P*Rxyz(:,2) + delta(2)) / voxel(2) + ny/2;
+z2 = (P*Rxyz(:,3) + delta(3)) / voxel(3) + nz/2;
 
 % partial derivatives of Rxyz wrt delta(4-6)
 Dx = [        -c6*s4,         -s4*s6,   -c4;
@@ -117,43 +128,46 @@ Dz = [        -c4*s6,          c4*c6,     0;
      -c5*c6-s4*s5*s6,-c5*s6+c6*s4*s5,     0;
       c6*s5-c5*s4*s6, c5*c6*s4+s5*s6,     0];
 
-% coordinates of im2 (centered at nx/2 ny/2 nz/2)
-x2 = nx/2 + delta(1) + P * Rxyz(:,1);
-y2 = ny/2 + delta(2) + P * Rxyz(:,2);
-z2 = nz/2 + delta(3) + P * Rxyz(:,3);
+%% mutual information by joint histogram estimation (hpv)
+function [fval grad] = hpv(im1,im2,delta,n,voxel)
 
-% early return for coordinates only
-if nargout>2
-    x2 = reshape(x2,[nx ny nz]);
-    y2 = reshape(y2,[nx ny nz]);
-    z2 = reshape(z2,[nx ny nz]);
-    fval = 0; grad = 0; return;
+[nx ny nz ns] = size(im1);
+
+if isa(im1,'gpuArray')
+    nx = gpuArray(nx);
+    ny = gpuArray(ny);
+    nz = gpuArray(nz);
 end
 
-% vectorize slice groups
-im1 = reshape(im1,[],ns);
-im2 = reshape(im2,[],ns);
+[x2 y2 z2 P Dx Dy Dz] = get_coords([nx ny nz],voxel,delta);
+
+% vectorize slab groups
+im1 = reshape(im1,nx*ny*nz,ns);
+im2 = reshape(im2,nx*ny*nz,ns);
+
+% joint histogram and partial derivatives [dx dy dz dxrot dyrot dzrot]
+h = zeros(n,n,7,'like',nx);
 
 % convolution with 4x4 Hann window
 for i = -1:2
     for j = -1:2
         for k = -1:2
 
-            xi = floor(x2)+i; % convolution x-index
-            yi = floor(y2)+j; % convolution y-index
-            zi = floor(z2)+k; % convolution z-index
+            ix = floor(x2)+i; % convolution x-index
+            iy = floor(y2)+j; % convolution y-index
+            iz = floor(z2)+k; % convolution z-index
 
-            dx = x2-xi;
-            dy = y2-yi;
-            dz = z2-zi;
+            dx = x2-ix;
+            dy = y2-iy;
+            dz = z2-iz;
 
-            xi = mod(xi,nx);
-            yi = mod(yi,ny);
-            zi = mod(zi,nz);
+            ix = mod(ix,nx);
+            iy = mod(iy,ny);
+            iz = mod(iz,nz);
 
             % histogram indices
-            ind = im2(1+xi+yi*nx+zi*nx*ny,:); 
-            ind = 1 + [im1(:) ind(:)]; clear xi yi zi
+            index = im2(1+ix+iy*nx+iz*nx*ny,:); clear ix iy iz
+            index = 1 + [im1(:) index(:)];
 
             % function (m=1) and partial derivatives (m=2-7)
             cosdx = cospi(dx/2); sindx = sinpi(dx/2); clear dx
@@ -161,22 +175,25 @@ for i = -1:2
             cosdz = cospi(dz/2); sindz = sinpi(dz/2); clear dz
 
             for m = 1:7
+
                 switch(m)
-                    case 1; f = (1+cosdx).*(1+cosdy).*(1+cosdz);      % f
-                    case 2; f = (0-sindx).*(1+cosdy).*(1+cosdz)*pi/2; % df/dx
-                    case 3; f = (1+cosdx).*(0-sindy).*(1+cosdz)*pi/2; % df/dy
-                    case 4; f = (1+cosdx).*(1+cosdy).*(0-sindz)*pi/2; % df/dz
-                    case 5; f = sum(F.*(P*Dx),2)*pi/180;              % df/dxrot
-                    case 6; f = sum(F.*(P*Dy),2)*pi/180;              % df/dyrot
-                    case 7; f = sum(F.*(P*Dz),2)*pi/180;              % df/dzrot
+                    case 1; f = (1+cosdx).*(1+cosdy).*(1+cosdz);               % f
+                    case 2; f = (0-sindx).*(1+cosdy).*(1+cosdz)*pi/2/voxel(1); % df/dx
+                    case 3; f = (1+cosdx).*(0-sindy).*(1+cosdz)*pi/2/voxel(2); % df/dy
+                    case 4; f = (1+cosdx).*(1+cosdy).*(0-sindz)*pi/2/voxel(3); % df/dz
+                    case 5; f = sum(F.*(P*Dx),2)*pi/180;                       % df/dxrot
+                    case 6; f = sum(F.*(P*Dy),2)*pi/180;                       % df/dyrot
+                    case 7; f = sum(F.*(P*Dz),2)*pi/180;                       % df/dzrot
                 end
 
                 % help with out-of-memory issues
                 if m==2; F(:,1) = f; clear sindx; end
-                if m==3; F(:,2) = f; clear sindy cosdz; end   
+                if m==3; F(:,2) = f; clear sindy cosdz; end
                 if m==4; F(:,3) = f; clear sindz cosdx cosdy; end
+                
+                if ns>1; f = repmat(f,ns,1); end
+                h(:,:,m) = h(:,:,m) + accumarray(index,f,[n n]);
 
-                h(:,:,m) = h(:,:,m) + accumarray(ind,repmat(f,ns,1),[n n]);
             end
 
         end
